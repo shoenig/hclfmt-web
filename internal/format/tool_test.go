c9984093d1d3a0de5826688d0154a73ff858fc3a
Seth Hoenig <shoenig@duck.com>
2023-04-05 23:19:26 -0500

    deps: purge testify in favor of shoenig/test

--- internal/format/tool_test.go
+++ internal/format/tool_test.go
@@ -7,40 +7,40 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/require"
+	"github.com/shoenig/test/must"
 )
 
 func load(t *testing.T, name string) io.ReadCloser {
 	f, err := os.Open(filepath.Join("resources", name))
-	require.NoError(t, err)
+	must.NoError(t, err)
 	return f
 }
 
 func TestTool_Process(t *testing.T) {
 	t.Run("basic.hcl", func(t *testing.T) {
 		result, err := NewTool(defaultMaxLen).Process(load(t, "basic.hcl"))
-		require.NoError(t, err)
-		require.False(t, result.Diagnostics.Problematic)
-		require.Equal(t, "no diagnostics", result.Diagnostics.Result)
-		require.Empty(t, result.Diagnostics.Body)
-		require.NotEmpty(t, result.Fmt)
+		must.NoError(t, err)
+		must.False(t, result.Diagnostics.Problematic)
+		must.Equal(t, "no diagnostics", result.Diagnostics.Result)
+		must.Empty(t, result.Diagnostics.Body)
+		must.NotEmpty(t, result.Fmt)
 	})
 
 	t.Run("complex.hcl", func(t *testing.T) {
 		result, err := NewTool(defaultMaxLen).Process(load(t, "complex.hcl"))
-		require.NoError(t, err)
-		require.False(t, result.Diagnostics.Problematic)
-		require.Equal(t, "no diagnostics", result.Diagnostics.Result)
-		require.Empty(t, result.Diagnostics.Body)
-		require.NotEmpty(t, result.Fmt)
+		must.NoError(t, err)
+		must.False(t, result.Diagnostics.Problematic)
+		must.Equal(t, "no diagnostics", result.Diagnostics.Result)
+		must.Empty(t, result.Diagnostics.Body)
+		must.NotEmpty(t, result.Fmt)
 	})
 
 	t.Run("problem.hcl", func(t *testing.T) {
 		result, err := NewTool(defaultMaxLen).Process(load(t, "problem.hcl"))
-		require.NoError(t, err)
-		require.True(t, result.Diagnostics.Problematic)
-		require.True(t, strings.HasPrefix(result.Diagnostics.Result, "<input>:2,18-22: Missing"))
-		require.NotEmpty(t, result.Diagnostics.Body)
-		require.Empty(t, result.Fmt)
+		must.NoError(t, err)
+		must.True(t, result.Diagnostics.Problematic)
+		must.True(t, strings.HasPrefix(result.Diagnostics.Result, "<input>:2,18-22: Missing"))
+		must.NotEmpty(t, result.Diagnostics.Body)
+		must.Empty(t, result.Fmt)
 	})
 }
